3fda9d8aa2d1723a8362bab4dac88f491dff5851
Nikolay Nikolaev <evrinoma@gmail.com>
2023-02-03 22:52:18 +0300

    install hooks

---
+++ tests/test.sh
@@ -0,0 +1,13 @@
+#/!bin/bash
+
+log=/tmp/test.dump
+echo > $log
+
+rm -rf vendor
+rm -rf composer.lock
+composer install --dev
+rm -rf /tmp/FetchBundle
+
+/usr/bin/php vendor/phpunit/phpunit/phpunit --bootstrap src/Tests/bootstrap.php --configuration phpunit.xml.dist src/Tests >> $log 2>&1
+status=$(cat $log | grep "ERRORS!")
+[ -z "$status" ] && exit 0 ||  exit -1
