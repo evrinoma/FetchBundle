cbbd627e7bb8d6914b78f8af0ce600e6154d37ac
Nikolay Nikolaev <evrinoma@gmail.com>
2023-04-08 08:34:58 +0300

    fixed temp path, composer and readme

--- tests/test.sh
+++ tests/test.sh
@@ -5,7 +5,7 @@ echo > $log
 
 rm -rf vendor
 rm -rf composer.lock
-composer install --dev
+COMPOSER_NO_DEV=0 composer
 rm -rf /tmp/FetchBundle
 
 /usr/bin/php vendor/phpunit/phpunit/phpunit --bootstrap src/Tests/bootstrap.php --configuration phpunit.xml.dist src/Tests >> $log 2>&1
